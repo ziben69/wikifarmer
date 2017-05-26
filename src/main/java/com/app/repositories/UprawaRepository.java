8c8d2bfa5a5ebde1efce68a8b221ffe8cb3157b4
Marcin Giec <giecmarcin@gmail.com>
2017-05-26 23:47:56 +0200

    //

--- src/main/java/com/app/repositories/UprawaRepository.java
+++ src/main/java/com/app/repositories/UprawaRepository.java
@@ -2,6 +2,11 @@ package com.app.repositories;
 
 import com.app.entities.Uprawa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UprawaRepository extends JpaRepository<Uprawa,Long> {
+    @Modifying
+    @Query(value="DELETE FROM region_uprawy WHERE uprawy_id = ?1", nativeQuery=true)
+    public void usunPowiazania(Long uprawyId);
 }
